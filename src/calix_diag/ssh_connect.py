a7f4b0338751c14014101a8dc041200f730a52a1
Victor Costa <victorcosta@cx6dx37krh-2.taileb8bf.ts.net>
2025-08-12 17:29:59 -0400

    added paramiko-expect

--- src/calix_diag/ssh_connect.py
+++ src/calix_diag/ssh_connect.py
@@ -1,8 +1,10 @@
 import paramiko 
+from paramiko_expect import SSHClientInteraction
 import time
+import typing
 import re
 
-def connect(k_path,jumphost,t_host,user,ont):
+def connect(k_path,jumphost,t_host: str,user,ont):
     diag_cmds = [
         f"show ont {ont}\n",
         f"show ont {ont} detail\n ",
@@ -23,23 +25,43 @@ def connect(k_path,jumphost,t_host,user,ont):
         username=user,
         pkey=ssh_key
     )
-    channel = jump_client.invoke_shell()
-    time.sleep(1)
-    if channel.recv_ready():
-        channel.send(f"logmein {t_host}\n")
-    time.sleep(5)
-    stdout = channel.recv(65535).decode(errors="ignore")
-    stdout = stdout.splitlines()
-    prompt = stdout[len(stdout) - 1]
-        
-    if re.match(r"[A-z]{3}\d{2}\-[A-z]{3}\d{2}\>",str(prompt)):
+    prompt = r"[A-z]{3}\d{2}\-[A-z]{3}\d{2}\>"
+    interact = SSHClientInteraction(jump_client,timeout=10,display=True)
+    interact.send(f"logmein {t_host}\n")
+    interact.expect(prompt, timeout=10)
+    if interact.last_match:
+        print(f"logged into {t_host}.")
         cmd_output = ""
         for e in diag_cmds:
-            channel.send(e)
-            time.sleep(2)
-            cmd_output += channel.recv(65535).decode(errors="ignore")
-            time.sleep(1)
+            interact.send(e)
+            interact.expect(prompt,timeout=5)
+            cmd_output += interact.current_output_clean
         print(cmd_output)
-
-    channel.close()
+    interact.close()
     jump_client.close()
+
+def connect_2(k_path: str,jumphost: str,user: str) -> paramiko.SSHClient:
+    if "25519" in k_path:
+        ssh_key = paramiko.Ed25519Key(filename=k_path)
+    else:
+        ssh_key = paramiko.RSAKey(filename=k_path)
+    jump_client = paramiko.SSHClient()
+    jump_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    jump_client.connect(
+        hostname=jumphost,
+        username=user,
+        pkey=ssh_key
+    )
+    return jump_client
+
+def calix_diag(ssh_client: paramiko.SSHClient,ont: str):
+    cmds = [
+        f"show ont {ont}\n",
+        f"show ont {ont} detail\n ",
+        f"show ont {ont} summary\n ",
+        f"show pm ont {ont} 1-day current\n",
+        f"show pm ont-port {ont}/g1 1-day current\n ",
+        f"show mac on-ont-port {ont}\n",
+        f"show lldp neighbor\n  "
+    ]
+    pass
