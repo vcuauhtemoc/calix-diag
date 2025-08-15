938f90cdb459ad1a44d31a3bc4ae8de11ae6360e
Victor Costa <victorcosta@cx6dx37krh-2.taileb8bf.ts.net>
2025-08-15 18:05:52 -0400

    created a general calix cmd wrapper

--- src/calix_diag/__main__.py
+++ src/calix_diag/__main__.py
@@ -1,6 +1,6 @@
 import argparse
 import pathlib as pl
-from .ssh_connect import jump_connect_2,calix_login,run_cmd
+from .ssh_connect import jump_connect_2,calix_login,calix_logout,run_cmd
 import time
 
 def main(argv=None):
@@ -11,26 +11,30 @@ def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("user", help="Your jumphost username")
     parser.add_argument("olthostname", help="OLT hostname")
-    parser.add_argument("uid", help="ONT UID")
+    # parser.add_argument("uid", help="ONT UID")
+    parser.add_argument("cmd", help="command to execute on OLT")
     args = parser.parse_args()
     hostname = args.olthostname
-    uid = args.uid
+    # uid = args.uid
     user = args.user
-    diag_cmds = [
-        f"show ont {uid}",
-        f"show ont {uid} detail",
-        f"show ont {uid} summary",
-        f"show pm ont {uid} 1-day current",
-        f"show pm ont-port {uid}/g1 1-day current",
-        f"show mac on-ont-port {uid}",
-        f"show lldp neighbor",
-        f"show log alarm"
-    ]
-    jump_connect_2("jump-jfk01.as46450.net",user,hostname)
+    cmd = args.cmd
+    # diag_cmds = [
+    #     f"show ont {uid}",
+    #     f"show ont {uid} detail",
+    #     f"show ont {uid} summary",
+    #     f"show pm ont {uid} 1-day current",
+    #     f"show pm ont-port {uid}/g1 1-day current",
+    #     f"show mac on-ont-port {uid}",
+    #     f"show lldp neighbor",
+    #     f"show log alarm"
+    # ]
+    jumphost = jump_connect_2("jump-jfk01.as46450.net",user,hostname)
     # jumphost = jump_connect(key_path,"jump-jfk01.as46450.net",user)
-    # ssh_interact = calix_login(jumphost,hostname)
+    calix_login(jumphost,hostname)
+    run_cmd(cmd,jumphost)
     # for e in diag_cmds:
-    #     run_cmd(e,ssh_interact)
+    #     run_cmd(e,jumphost)
+    calix_logout(jumphost)
 
 
 if __name__ == "__main__":
--- src/calix_diag/ssh_connect.py
+++ src/calix_diag/ssh_connect.py
@@ -1,8 +1,9 @@
 import paramiko 
 from paramiko_expect import SSHClientInteraction
 import sys
+from types import SimpleNamespace
 from pexpect import pxssh
-prompt = r"[A-Za-z]{3}\d{2}\-[A-Za-z]{3}\d{2}\>\s*$"
+calix_prompt = r"[A-Za-z]{3}\d{2}\-[A-Za-z]{3}\d{2}\>\s*$"
 
 def jump_connect(k_path: str,jumphost: str,user: str) -> paramiko.SSHClient:
     if "25519" in k_path:
@@ -20,29 +21,33 @@ def jump_connect(k_path: str,jumphost: str,user: str) -> paramiko.SSHClient:
 
 def jump_connect_2(jumphost: str,user: str,t_host):
     jump_client = pxssh.pxssh()
-    jump_client.login(jumphost,user,ssh_key=True)
-    print(str(jump_client.after))
-    jump_client.sendline(f"logmein {t_host}")
-    jump_client.prompt()
-    print(str(jump_client.before))
-    jump_client.sendline("exit")
-    jump_client.prompt()
-    print(str(jump_client.before))
-    jump_client.logout()
+    try:
+        jump_client.login(jumphost,user,ssh_key=True)
+        return jump_client
+    except Exception as ex: # turn this into raise
+        jump_client.logout()
+        for e in ex.args:
+            print(e)
+        
+        
 
-def calix_login(jc,t_host):
-    interact = SSHClientInteraction(jc,timeout=10,display=True)
-    interact.send(f"logmein {t_host}\n")
-    interact.expect(prompt,timeout=10)
+def calix_login(cx_prompt: pxssh.pxssh,t_host):
+    cx_prompt.sendline(f"logmein {t_host}\n")
+    cx_prompt.expect(calix_prompt,timeout=5)
     print(f"\nLogged into {t_host}.\n")
-    return interact
+    return cx_prompt
 
-def run_cmd(cmd:str,interact:SSHClientInteraction):
-    interact.send(cmd,newline="\r\n")
-    interact.expect([prompt,"--MORE--"], timeout=10)
-    print(interact.current_output.splitlines()[len(interact.current_output.splitlines()) - 1])
-    print("* * *")
-    while interact.last_match == "--MORE--":
+def calix_logout(cx_prompt: pxssh.pxssh):
+    cx_prompt.sendline("exit")
+    return cx_prompt
+
+
+def run_cmd(cmd:str,interact:pxssh.pxssh):
+    interact.sendline(cmd)
+    p_match = interact.expect([calix_prompt,r"\r\n"], timeout=10)
+    print(interact.before.decode("utf-8"))
+    while p_match == 1:
         interact.send(" ")
-        interact.expect([prompt,"--MORE--"], timeout=10)
+        print(interact.before.decode("utf-8"))        
+        interact.expect([calix_prompt,"\r\n"], timeout=10)
     return None
